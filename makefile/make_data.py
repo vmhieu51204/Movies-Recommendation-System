3d6cbbaf0a3c988291dc78403b51a645c0ab339e
vmhieu51204 <126134607+lskjbvlioaub@users.noreply.github.com>
2025-05-12 21:00:19 +0700

    -update 1

--- makefile/make_data.py
+++ makefile/make_data.py
@@ -31,20 +31,21 @@ movies = movies.drop(columns=columns_drop)
 movies.to_csv(os.path.join(path, 'p_movies.csv'))
 
 movie_ids_in_ratings = ratings['movieId'].unique()
-print(f"Number of movie IDs in ratings dataframe: {len(movie_ids_in_ratings)}")
+#print(f"Number of movie IDs in ratings dataframe: {len(movie_ids_in_ratings)}")
 movie_ids_in_movies = movies['id'].unique()
-print(f"Number of movie IDs in movies dataframe: {len(movie_ids_in_movies)}")
+#print(f"Number of movie IDs in movies dataframe: {len(movie_ids_in_movies)}")
 
 missing_ids_ratings = [movie_id for movie_id in movie_ids_in_ratings 
                      if movie_id not in movie_ids_in_movies]
 missing_ids_movies = [movie_id for movie_id in movie_ids_in_movies 
                      if movie_id not in movie_ids_in_ratings]
 
-print(f"Number of movie IDs present in ratings but missing from movies: {len(missing_ids_ratings)}")
-print(f"Percentage of missing movies: {len(missing_ids_ratings) / len(movie_ids_in_ratings) * 100:.2f}%")
+#print(f"Number of movie IDs present in ratings but missing from movies: {len(missing_ids_ratings)}")
+#print(f"Percentage of missing movies: {len(missing_ids_ratings) / len(movie_ids_in_ratings) * 100:.2f}%")
 
-print(f"Number of movie IDs present in movies but missing from ratings: {len(missing_ids_movies)}")
-print(f"Percentage of missing movies: {len(missing_ids_movies) / len(movie_ids_in_movies) * 100:.2f}%")
+#print(f"Number of movie IDs present in movies but missing from ratings: {len(missing_ids_movies)}")
+#print(f"Percentage of missing movies: {len(missing_ids_movies) / len(movie_ids_in_movies) * 100:.2f}%")
+
 # Ensure consistent types
 movies['id'] = movies['id'].astype(int)
 ratings['movieId'] = ratings['movieId'].astype(int)
@@ -52,7 +53,7 @@ ratings['movieId'] = ratings['movieId'].astype(int)
 valid_movie_ids = set(movies['id'])  # Convert to set for fast lookup
 ratings = ratings[ratings['movieId'].isin(valid_movie_ids)]
 
-print(f"New number of rows in ratings after removing invalid movie IDs: {len(ratings)}")
+#print(f"New number of rows in ratings after removing invalid movie IDs: {len(ratings)}")
 
 ratings.drop(columns=['timestamp'], inplace=True)
-ratings.to_csv('/kaggle/working/ratings.csv')
+ratings.to_csv(os.path.join(path, 'p_ratings.csv'))
---
+++ model/matrix_factorization.py
@@ -0,0 +1,6 @@
+from surprise import Dataset, Reader
+from surprise.model_selection import train_test_split
+from surprise import Reader, Dataset, SVD, SlopeOne, accuracy
+
+import numpy as np 
+import pandas as pd 
